99543b2c9164eb29523ccc4b653b19f480e085e4
codemine24 <codemine24@gmail.com>
2025-05-19 22:43:45 +0600

    DialogStaticPreview

--- src/app/(content)/elements/view/dialog-view.tsx
+++ src/app/(content)/elements/view/dialog-view.tsx
@@ -9,186 +9,188 @@ import { SectionTitle } from "@/components/core/section-title";
 import { CodePreviewCopyWrapper } from "@/components/code-preview-copy-wrapper";
 
 import {
-    DialogCustomBackdropPreview,
-    DialogPlacementPreview,
-    DialogSizesPreview,
-    DialogStaticPreview,
-    DialogConfirmation1Preview,
-    DialogConfirmation2Preview,
-    DialogMultiStepPreview,
-    DialogScrollablePreview,
-    DialogFullScreenPreview,
-    DialogAnimationPreview,
-    DialogHeaderFooterPreview,
-    DialogImageViewPreview,
-    DialogSignInPreview,
-    DialogCTAPreview,
-    DialogWelcomePreview,
+  DialogCustomBackdropPreview,
+  DialogPlacementPreview,
+  DialogSizesPreview,
+  DialogStaticPreview,
+  DialogConfirmation1Preview,
+  DialogConfirmation2Preview,
+  DialogMultiStepPreview,
+  DialogScrollablePreview,
+  DialogFullScreenPreview,
+  DialogAnimationPreview,
+  DialogHeaderFooterPreview,
+  DialogImageViewPreview,
+  DialogSignInPreview,
+  DialogCTAPreview,
+  DialogWelcomePreview,
 } from "../components/dialog";
 import {
-    dialogCustomBackdropString,
-    dialogPlacementString,
-    dialogSizesString,
-    dialogStaticString,
-    dialogConfirmation1String,
-    dialogConfirmation2String,
-    dialogMultiStepString,
-    dialogScrollableString,
-    dialogFullScreenString,
-    dialogAnimationString,
-    dialogHeaderFooterString,
-    dialogImageViewPreviewString,
-    dialogSignInString,
-    dialogCTAString,
-    dialogWelcomeString,
+  dialogCustomBackdropString,
+  dialogPlacementString,
+  dialogSizesString,
+  dialogStaticString,
+  dialogConfirmation1String,
+  dialogConfirmation2String,
+  dialogMultiStepString,
+  dialogScrollableString,
+  dialogFullScreenString,
+  dialogAnimationString,
+  dialogHeaderFooterString,
+  dialogImageViewPreviewString,
+  dialogSignInString,
+  dialogCTAString,
+  dialogWelcomeString,
 } from "../code-string/dialog";
 
 const sections = [
-    {
-        id: "dialog-sizes",
-        title: "Sizes",
-        description: "Native material ui dialog with solid background",
-        codeString: dialogSizesString,
-        preview: <DialogSizesPreview />,
-    },
-    {
-        id: "dialog-placement",
-        title: "Placement",
-        description: "Native material ui dialog with solid background",
-        codeString: dialogPlacementString,
-        preview: <DialogPlacementPreview />,
-    },
-    {
-        id: "dialog-static",
-        title: "Static",
-        description: "Native material ui dialog with solid background",
-        codeString: dialogStaticString,
-        preview: <DialogStaticPreview />,
-    },
-    {
-        id: "dialog-custom-backdrop",
-        title: "Custom Backdrop",
-        description: "Native material ui dialog with solid background",
-        codeString: dialogCustomBackdropString,
-        preview: <DialogCustomBackdropPreview />,
-    },
-    {
-        id: "dialog-confirmation",
-        title: "Confirmation",
-        description: "Native material ui dialog with solid background",
-        codeString: dialogConfirmation1String,
-        preview: <DialogConfirmation1Preview />,
-    },
-    {
-        id: "dialog-confirmation-2",
-        title: "Confirmation 2",
-        description: "Native material ui dialog with solid background",
-        codeString: dialogConfirmation2String,
-        preview: <DialogConfirmation2Preview />,
-    },
-    {
-        id: "dialog-multi-step",
-        title: "Multi Step",
-        description: "Native material ui dialog with solid background",
-        codeString: dialogMultiStepString,
-        preview: <DialogMultiStepPreview />,
-    },
-    {
-        id: "dialog-scrollable",
-        title: "Scrollable",
-        description: "Native material ui dialog with solid background",
-        codeString: dialogScrollableString,
-        preview: <DialogScrollablePreview />,
-    },
-    {
-        id: "dialog-full-screen",
-        title: "Full Screen",
-        description: "Native material ui dialog with solid background",
-        codeString: dialogFullScreenString,
-        preview: <DialogFullScreenPreview />,
-    },
-    {
-        id: "dialog-animation",
-        title: "Animation",
-        description: "Native material ui dialog with solid background",
-        codeString: dialogAnimationString,
-        preview: <DialogAnimationPreview />,
-    },
-    {
-        id: "dialog-header-footer",
-        title: "Header Footer",
-        description: "Native material ui dialog with solid background",
-        codeString: dialogHeaderFooterString,
-        preview: <DialogHeaderFooterPreview />,
-    },
-    {
-        id: "dialog-image-view",
-        title: "Image View",
-        description: "Native material ui dialog with solid background",
-        codeString: dialogImageViewPreviewString,
-        preview: <DialogImageViewPreview />,
-    },
-    {
-        id: "dialog-signin",
-        title: "Sign In",
-        description: "Native material ui dialog with solid background",
-        codeString: dialogSignInString,
-        preview: <DialogSignInPreview />,
-    },
-    {
-        id: "dialog-cta",
-        title: "CTA",
-        description: "Native material ui dialog with solid background",
-        codeString: dialogCTAString,
-        preview: <DialogCTAPreview />,
-    },
-    {
-        id: "dialog-welcome",
-        title: "Welcome",
-        description: "Native material ui dialog with solid background",
-        codeString: dialogWelcomeString,
-        preview: <DialogWelcomePreview />,
-    },
-]
-
+  {
+    id: "dialog-sizes",
+    title: "Different sizes dialog",
+    description:
+      "Choose your desire dialog four different sizes given by material ui. ",
+    codeString: dialogSizesString,
+    preview: <DialogSizesPreview />,
+  },
+  {
+    id: "dialog-placement",
+    title: "Different placement",
+    description:
+      "We have 5 different position for a dialog. Top-left, top-right, bottom-left, bottom-right and center. ",
+    codeString: dialogPlacementString,
+    preview: <DialogPlacementPreview />,
+  },
+  {
+    id: "dialog-static",
+    title: "Static dialog",
+    description:
+      "This dialog will not close on outside click. If you need a dialog that will not close on outside click, use this.",
+    codeString: dialogStaticString,
+    preview: <DialogStaticPreview />,
+  },
+  {
+    id: "dialog-custom-backdrop",
+    title: "Custom Backdrop",
+    description: "Native material ui dialog with solid background",
+    codeString: dialogCustomBackdropString,
+    preview: <DialogCustomBackdropPreview />,
+  },
+  {
+    id: "dialog-confirmation",
+    title: "Confirmation",
+    description: "Native material ui dialog with solid background",
+    codeString: dialogConfirmation1String,
+    preview: <DialogConfirmation1Preview />,
+  },
+  {
+    id: "dialog-confirmation-2",
+    title: "Confirmation 2",
+    description: "Native material ui dialog with solid background",
+    codeString: dialogConfirmation2String,
+    preview: <DialogConfirmation2Preview />,
+  },
+  {
+    id: "dialog-multi-step",
+    title: "Multi Step",
+    description: "Native material ui dialog with solid background",
+    codeString: dialogMultiStepString,
+    preview: <DialogMultiStepPreview />,
+  },
+  {
+    id: "dialog-scrollable",
+    title: "Scrollable",
+    description: "Native material ui dialog with solid background",
+    codeString: dialogScrollableString,
+    preview: <DialogScrollablePreview />,
+  },
+  {
+    id: "dialog-full-screen",
+    title: "Full Screen",
+    description: "Native material ui dialog with solid background",
+    codeString: dialogFullScreenString,
+    preview: <DialogFullScreenPreview />,
+  },
+  {
+    id: "dialog-animation",
+    title: "Animation",
+    description: "Native material ui dialog with solid background",
+    codeString: dialogAnimationString,
+    preview: <DialogAnimationPreview />,
+  },
+  {
+    id: "dialog-header-footer",
+    title: "Header Footer",
+    description: "Native material ui dialog with solid background",
+    codeString: dialogHeaderFooterString,
+    preview: <DialogHeaderFooterPreview />,
+  },
+  {
+    id: "dialog-image-view",
+    title: "Image View",
+    description: "Native material ui dialog with solid background",
+    codeString: dialogImageViewPreviewString,
+    preview: <DialogImageViewPreview />,
+  },
+  {
+    id: "dialog-signin",
+    title: "Sign In",
+    description: "Native material ui dialog with solid background",
+    codeString: dialogSignInString,
+    preview: <DialogSignInPreview />,
+  },
+  {
+    id: "dialog-cta",
+    title: "CTA",
+    description: "Native material ui dialog with solid background",
+    codeString: dialogCTAString,
+    preview: <DialogCTAPreview />,
+  },
+  {
+    id: "dialog-welcome",
+    title: "Welcome",
+    description: "Native material ui dialog with solid background",
+    codeString: dialogWelcomeString,
+    preview: <DialogWelcomePreview />,
+  },
+];
 
 export const DialogView = () => {
-    const { setSections } = useOnThisPage();
+  const { setSections } = useOnThisPage();
 
-    React.useEffect(() => {
-        setSections(sections);
-    }, [setSections]);
+  React.useEffect(() => {
+    setSections(sections);
+  }, [setSections]);
 
-    return (
-        <Box>
-            <CustomBreadCrumbs
-                pathArr={[
-                    { label: "Elements", path: "/elements" },
-                    { label: "Dialog", path: "" },
-                ]}
-            />
-            <PageTitle
-                title="Dialog"
-                description="Material UI core dialog with extended design, and functionality. "
-            />
+  return (
+    <Box>
+      <CustomBreadCrumbs
+        pathArr={[
+          { label: "Elements", path: "/elements" },
+          { label: "Dialog", path: "" },
+        ]}
+      />
+      <PageTitle
+        title="Dialog"
+        description="Material UI core dialog with extended design, and functionality. "
+      />
 
-            <Divider sx={{ my: 4 }} />
+      <Divider sx={{ my: 4 }} />
 
-            {/* Dialog - 1 */}
-            {sections.map((section) => (
-                <Box key={section.id} sx={{ mb: 4 }}>
-                    {" "}
-                    <SectionTitle
-                        title={section.title}
-                        description={section.description || ""}
-                        id={section.id}
-                    />
-                    <CodePreviewCopyWrapper
-                        codeString={section.codeString}
-                        preview={section.preview}
-                    />
-                </Box>
-            ))}
+      {/* Dialog - 1 */}
+      {sections.map((section) => (
+        <Box key={section.id} sx={{ mb: 4 }}>
+          {" "}
+          <SectionTitle
+            title={section.title}
+            description={section.description || ""}
+            id={section.id}
+          />
+          <CodePreviewCopyWrapper
+            codeString={section.codeString}
+            preview={section.preview}
+          />
         </Box>
-    );
+      ))}
+    </Box>
+  );
 };
