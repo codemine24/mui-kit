193e4215b1839f77b8280cbc96da6e4a070d1e3c
RakibMojumder <sujonahmed45a4@gmail.com>
2025-05-20 11:12:48 +0600

    dialog responsive issue solved

--- src/app/(content)/elements/components/dialog/dialog-image-view-preview.tsx
+++ src/app/(content)/elements/components/dialog/dialog-image-view-preview.tsx
@@ -63,9 +63,9 @@ export const DialogImageViewPreview = () => {
                     {/* Image */}
                     <CardMedia
                         component="img"
-                        height={420}
                         image="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2717&q=80"
                         alt="Mountain landscape"
+                        sx={{ height: { xs: 300, sm: 420 } }}
                     />
 
                     {/* Stats and Share */}
