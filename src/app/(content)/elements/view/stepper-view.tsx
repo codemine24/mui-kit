4d99fa2470ac3b7ab633b1594c04483fe52b2831
RakibMojumder <sujonahmed45a4@gmail.com>
2025-05-26 17:11:26 +0600

    test

--- src/app/(content)/elements/view/stepper-view.tsx
+++ src/app/(content)/elements/view/stepper-view.tsx
@@ -40,6 +40,8 @@ export const StepperView = () => {
                 description="Material UI core stepper with extended design, and functionality. "
             />
 
+            <Divider sx={{ my: 4 }} />
+
             {/* Stepper Section */}
             {
                 sections.map((section) => (
