2ae9df39b5225a561269c0329936c28e9ac04ac1
fazlyalahi01 <fazlyalahi.ru@gmail.com>
2025-05-29 18:45:16 +0600

    video popover added.

--- src/app/(content)/blocks/components/about/about-video-preview.tsx
+++ src/app/(content)/blocks/components/about/about-video-preview.tsx
@@ -1,21 +1,54 @@
-import { Box, Grid, Typography } from "@mui/material";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import { Box, Dialog, Grid, Typography } from "@mui/material";
+import { useState } from "react";
 
 export const AboutVideoPreview = () => {
+  const [open, setOpen] = useState(false);
+
+  const handlePlayClick = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <Box>
       <Grid container>
-        <Grid size={{ xs: 12, md: 6 }}>
+        <Grid size={{ xs: 12, md: 6 }} sx={{ position: "relative" }}>
           <Box
-            component={"img"}
+            component="img"
             src="https://images.unsplash.com/photo-1543269865-cbf427effbad?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             sx={{
               width: "100%",
               height: "100%",
               objectFit: "cover",
-              //   borderRadius: 2,
+              display: "block",
             }}
           />
+
+          <Box
+            sx={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              backgroundColor: "rgba(0, 0, 0, 0.4)",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              cursor: "pointer",
+            }}
+            onClick={handlePlayClick}
+          >
+            <PlayCircleOutlineIcon
+              sx={{
+                fontSize: 64,
+                color: "#fff",
+                transition: "0.3s",
+                "&:hover": { color: "#e60077" },
+              }}
+            />
+          </Box>
         </Grid>
+
         <Grid
           size={{ xs: 12, md: 6 }}
           sx={{ p: { xs: 2, md: 4 }, background: "#5B9279" }}
@@ -79,10 +112,34 @@ export const AboutVideoPreview = () => {
             quaerat sapiente laborum in nisi excepturi ipsam totam non nemo
             voluptatum, architecto.
           </Typography>
-
-            
         </Grid>
       </Grid>
+
+      {/* DIALOG */}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        maxWidth="md"
+        fullWidth
+        PaperProps={{
+          sx: {
+            backgroundColor: "black",
+            position: "relative",
+          },
+        }}
+      >
+        <Box
+          component="video"
+          src="https://www.w3schools.com/html/mov_bbb.mp4"
+          controls
+          autoPlay
+          sx={{
+            width: "100%",
+            height: "auto",
+            outline: "none",
+          }}
+        />
+      </Dialog>
     </Box>
   );
 };
