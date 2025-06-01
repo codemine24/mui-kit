afb5b8159cfce83748ec88b4847b1b005198547f
codemine <codemine24@gmail.com>
2025-06-01 17:34:58 +0600

    chagelog codestring added

---
+++ src/app/(content)/elements/components/timeline/timeline-company-milestone-preview.tsx
@@ -0,0 +1,9 @@
+import { Timeline } from "@mui/lab";
+
+export function TimelineCompanyMilestonePreview() {
+    return (
+        <Timeline position="right" sx={{ mt: 4, width: "100%" }}>
+
+        </Timeline>
+    );
+}
