a416e7cc14c1883d928373ee6162c1f320f30914
fazlyalahi01 <fazlyalahi.ru@gmail.com>
2025-06-05 12:12:24 +0600

    google ta manager added.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Box, CssBaseline, GlobalStyles } from "@mui/material";
 import { Metadata } from "next";
 import { JetBrains_Mono, Roboto } from "next/font/google";
 import "../styles/globals.css";
+import Script from "next/script";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -26,7 +27,7 @@ export const metadata: Metadata = {
     process.env.NEXT_PUBLIC_PRODUCTION_URL || "https://www.muikit.com"
   ),
   title: {
-    default: "MUI Kit - Open Source MUI Components library",
+    default: "MUI Kit - Open Source Material UI Components library",
     template: "%s | MUI Kit",
   },
   description: "A complete set of open source components for Material UI",
@@ -43,11 +44,35 @@ export default function RootLayout({
       className={`${roboto.variable} ${jetBrainsMono.variable}`}
       suppressHydrationWarning
     >
+      <head>
+        {/* GTM Script */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','G-FK9FFDPDJG');
+            `,
+          }}
+        />
+      </head>
       <body>
+        {/* GTM NoScript fallback */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=G-FK9FFDPDJG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <AppProvider>
           <CssBaseline />
-
-          {/* Global transition styles */}
           <GlobalStyles
             styles={{
               "html, body, #__next": {
@@ -59,13 +84,10 @@ export default function RootLayout({
               },
             }}
           />
-
           <OnThisPageProvider>
             <TopHeader />
             <Header />
-
             <Box>{children}</Box>
-
             <Footer />
           </OnThisPageProvider>
         </AppProvider>
