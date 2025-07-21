c0af1b91b119798099dedc459277cf960c663ce9
devsman-forhad <forhadhossain357471@gmail.com>
2025-07-21 16:38:06 +0600

    Added a new tab section with horizontal scrollable

---
+++ src/app/(content)/elements/components/tab/Tab-7-preview.tsx
@@ -0,0 +1,42 @@
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import HomeIcon from "@mui/icons-material/Home";
+import SettingsIcon from "@mui/icons-material/Settings";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import * as React from "react";
+
+export default function Tab7Preview() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    if (newValue >= 0 && newValue <= 2) {
+      setValue(newValue);
+    }
+  };
+
+  return (
+    <Tabs
+      value={value}
+      onChange={handleChange}
+      aria-label="icon position tabs example"
+      variant="scrollable"
+      scrollButtons="auto"
+    >
+      <Tab icon={<HomeIcon />} iconPosition="start" label="Home" />
+      <Tab icon={<AccountCircleIcon />} iconPosition="start" label="Profile" />
+      <Tab icon={<SettingsIcon />} iconPosition="start" label="Setting" />
+      <Tab icon={<HomeIcon />} iconPosition="start" label="Home" />
+      <Tab icon={<AccountCircleIcon />} iconPosition="start" label="Profile" />
+      <Tab icon={<SettingsIcon />} iconPosition="start" label="Setting" />
+      <Tab icon={<HomeIcon />} iconPosition="start" label="Home" />
+      <Tab icon={<AccountCircleIcon />} iconPosition="start" label="Profile" />
+      <Tab icon={<SettingsIcon />} iconPosition="start" label="Setting" />
+      <Tab icon={<HomeIcon />} iconPosition="start" label="Home" />
+      <Tab icon={<AccountCircleIcon />} iconPosition="start" label="Profile" />
+      <Tab icon={<SettingsIcon />} iconPosition="start" label="Setting" />
+      <Tab icon={<HomeIcon />} iconPosition="start" label="Home" />
+      <Tab icon={<AccountCircleIcon />} iconPosition="start" label="Profile" />
+      <Tab icon={<SettingsIcon />} iconPosition="start" label="Setting" />
+    </Tabs>
+  );
+}
