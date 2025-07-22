b35e4f8df82410f233c20954b622e526701ad7a1
devsman-forhad <forhadhossain357471@gmail.com>
2025-07-22 09:37:17 +0600

    Added a new colore tab

--- src/app/(content)/elements/components/tab/Tab-4-preview.tsx
+++ src/app/(content)/elements/components/tab/Tab-4-preview.tsx
@@ -1,9 +1,17 @@
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import HomeIcon from "@mui/icons-material/Home";
 import SettingsIcon from "@mui/icons-material/Settings";
+import { styled } from "@mui/material/styles";
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
 import * as React from "react";
+
+const CustomTab = styled(Tab)({
+  "&.MuiButtonBase-root": {
+    paddingTop: 0,
+    paddingBottom: 10,
+  },
+});
 
 export default function Tab4Preview() {
   const [value, setValue] = React.useState(0);
@@ -18,9 +26,13 @@ export default function Tab4Preview() {
       onChange={handleChange}
       aria-label="icon position tabs example"
     >
-      <Tab icon={<HomeIcon />} iconPosition="start" label="Home" />
-      <Tab icon={<AccountCircleIcon />} iconPosition="start" label="Profile" />
-      <Tab icon={<SettingsIcon />} iconPosition="start" label="Setting" />
+      <CustomTab icon={<HomeIcon />} iconPosition="start" label="Home" />
+      <CustomTab
+        icon={<AccountCircleIcon />}
+        iconPosition="start"
+        label="Profile"
+      />
+      <CustomTab icon={<SettingsIcon />} iconPosition="start" label="Setting" />
     </Tabs>
   );
 }
