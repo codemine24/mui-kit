85e0e0f2de1a8a165c8db9e51abed5d7282f082c
Forhad Hossain <forhadhossain357471@gmail.com>
2025-07-30 12:18:54 +0600

    Fixed some duplication issues

--- src/app/(content)/elements/components/chip/chip-custom-number-preview.tsx
+++ src/app/(content)/elements/components/chip/chip-custom-number-preview.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Chip, { chipClasses } from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
-import { alpha } from "@mui/system";
 
 export const ChipCustomNumberPreview = () => {
   return (
