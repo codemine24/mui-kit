e7f90dd3f71c438c86a7b6be5824ef553885162a
RakibMojumder <sujonahmed45a4@gmail.com>
2025-07-31 17:20:12 +0600

    skeleton without content variant added

--- src/app/(content)/elements/components/skeleton/index.ts
+++ src/app/(content)/elements/components/skeleton/index.ts
@@ -2,3 +2,4 @@ export * from "./skeleton-basic-preview";
 export * from "./skeleton-animation-preview";
 export * from "./skeleton-squire-block-preview";
 export * from "./skeleton-social-post-preview";
+export * from "./skeleton-without-content";
--- src/app/(content)/elements/view/skeleton-view.tsx
+++ src/app/(content)/elements/view/skeleton-view.tsx
@@ -8,7 +8,13 @@ import { PATHS } from "@/router/paths";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import React from "react";
-import { SkeletonBasicPreview, SkeletonAnimationPreview, SkeletonSquireBlockPreview, SkeletonSocialPostPreview } from "../components/skeleton";
+import {
+    SkeletonBasicPreview,
+    SkeletonAnimationPreview,
+    SkeletonSquireBlockPreview,
+    SkeletonSocialPostPreview,
+    SkeletonWithoutContent
+} from "../components/skeleton";
 
 const sections = [
     {
@@ -42,6 +48,14 @@ const sections = [
             "A basic customizable skeleton component built with Material UI.",
         codeString: "",
         preview: <SkeletonSocialPostPreview />,
+    },
+    {
+        id: "skeleton-5",
+        title: "Without Content MUI Skeleton",
+        description:
+            "A basic customizable skeleton component built with Material UI.",
+        codeString: "",
+        preview: <SkeletonWithoutContent />,
     },
 ];
 
